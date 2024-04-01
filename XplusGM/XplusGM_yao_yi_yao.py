e563c5d6a4887787adedc12485d00af196bb6cd4
SwartzMss <swartz_lubel@outlook.com>
2024-04-01 19:42:14 +0800

    aa
    Signed-off-by: SwartzMss <swartz_lubel@outlook.com>

--- XplusGM/XplusGM_yao_yi_yao.py
+++ XplusGM/XplusGM_yao_yi_yao.py
@@ -251,5 +251,6 @@ if __name__ == "__main__":
         #app.run("MSS", 2,"emulator-5558")
         #app.run("SME1102", 1,"emulator-5556")
         #app.run("SMD01", 6,"emulator-5566")
-        app.run("SMD02", 7,"emulator-5568")
+        #app.run("SMD02", 7,"emulator-5568")
+        app.run("SBM", 4,"emulator-5562")
         time.sleep(3)
